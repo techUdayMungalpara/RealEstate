c1330cd351df4567e7eedf1ea27cb8b46e54652b
techUdayMungalpara <techudaymungalpara@gmail.com>
2023-04-04 15:47:49 +0530

    RealEstate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Banner from "../components/Banner";
 import HouseList from "./../components/HouseList";
 const Home = () => {
+  // https://www.youtube.com/watch?v=CHe_QJcTK5Y&ab_channel=CristianMihai
   return (
     <div className="min-h-[1800px]  ">
       <Banner />
